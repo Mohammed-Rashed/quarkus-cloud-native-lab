152d480fdc93b7e93b1a2fed86108c3ee1565b64
mohammed-rashed <jstrasshed@gmail.com>
2026-09-18 03:24:44 +0300

    Enhance CreateOrderDto with validation and update OrderService to use record

--- src/main/java/com/mohammed/order/OrderService.java
+++ src/main/java/com/mohammed/order/OrderService.java
@@ -3,15 +3,16 @@ package com.mohammed.order;
 import com.mohammed.order.dto.CreateOrderDto;
 import com.mohammed.order.dto.OrderResponseDto;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.validation.Valid;
 
 @ApplicationScoped
 public class OrderService {
 
-    public OrderResponseDto create(CreateOrderDto dto){
+    public OrderResponseDto create(@Valid CreateOrderDto dto){
         return  new OrderResponseDto(
                 1L,
-                dto.product,
-                dto.quantity,
+                dto.product(),
+                dto.quantity(),
                 "Pending"
         );
     }
--- src/main/java/com/mohammed/order/dto/CreateOrderDto.java
+++ src/main/java/com/mohammed/order/dto/CreateOrderDto.java
@@ -1,6 +1,14 @@
 package com.mohammed.order.dto;
 
-public class CreateOrderDto {
-    public String product;
-    public int quantity;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateOrderDto (
+        @NotBlank(message = "Product is required")
+        String product,
+
+        @Min(value = 1, message = "Quantity must be at least 1")
+        int quantity
+){
+
 }
