b1adaed396879c0310d6f60c26a63e0677fd3d0f
mohammed-rashed <jstrasshed@gmail.com>
2026-09-22 04:57:45 +0300

    Implement validation error handling in OrderNotFoundExceptionMapper

--- src/main/java/com/mohammed/order/exception/OrderNotFoundExceptionMapper.java
+++ src/main/java/com/mohammed/order/exception/OrderNotFoundExceptionMapper.java
@@ -1,20 +1,45 @@
 package com.mohammed.order.exception;
 
 import com.mohammed.order.dto.ErrorResponseDto;
+import com.mohammed.order.dto.ValidationErrorResponseDto;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Provider
-public class OrderNotFoundExceptionMapper implements ExceptionMapper<OrderNotFoundException> {
+public class OrderNotFoundExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
     @Override
-    public Response toResponse(OrderNotFoundException exception) {
+    public Response toResponse(ConstraintViolationException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getConstraintViolations()
+                .forEach(violation -> {
+
+
+                    String path = violation.getPropertyPath().toString();
+
+                    String field = path.substring(
+                            path.lastIndexOf('.') + 1
+                    );
+                    String message = violation.getMessage();
+
+                    errors.put(field, message);
+                });
+
+        ValidationErrorResponseDto response =
+                new ValidationErrorResponseDto(
+                        400,
+                        "Validation failed",
+                        errors
+                );
+
         return Response
-                .status(Response.Status.NOT_FOUND)
-                .entity(new ErrorResponseDto(
-                        404,
-                        exception.getMessage()
-                ))
+                .status(Response.Status.BAD_REQUEST)
+                .entity(response)
                 .build();
     }
 }
